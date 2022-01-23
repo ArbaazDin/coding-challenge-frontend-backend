87046fb436646a458ac2fdefa3740882e5b4df1a
ArbaazDin <arbaazdin@gmail.com>
2022-01-23 21:24:38 +0530

    changed secret keys and one api token

--- backend/common/generateJwt.ts
+++ backend/common/generateJwt.ts
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 
-const secretKey = "A723678klnvdfk32";
+const secretKey = "Sjis6789GGvda";
 
 export const generateUserJwt = (userId, name, email) => {
     return jwt.sign({ userId: userId, name: name, type: "USER", email: email }, secretKey, { expiresIn: '30d' });
--- backend/repositories/characterRepository.ts
+++ backend/repositories/characterRepository.ts
@@ -5,7 +5,7 @@ export const characterRepository = {
     getAllCharacters() {
         const config: AxiosRequestConfig = {
             headers: {
-                "Authorization": "Bearer LydyUGtvZmriz7Yl4Ylg",
+                "Authorization": "Bearer KQeV0JegBT-ZTcp4JyK6",
             }
         };
         return axios.get('https://the-one-api.dev/v2/character', config);
